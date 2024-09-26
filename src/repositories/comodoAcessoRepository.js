020d25fcffa2bb2c6cf494f1999d2a36b774accc
KevinRomRib <romribkevin@gmail.com>
2024-09-26 18:50:36 -0300

    Criando CRUD de comodo acesso

---
+++ src/repositories/comodoAcessoRepository.js
@@ -0,0 +1,39 @@
+const database = require('../config/database');
+const table = 'tb_comodo_acesso';
+
+module.exports = {
+  find: async (params) => {
+    const findParams = [];
+
+    if (params.hasOwnProperty('id_acesso')) {
+      findParams.push('tb_comodo_acesso.id_acesso = ' + params.id_acesso);
+    }
+
+    const query = database(table).select('*').first();
+
+    if (findParams.length > 0) {
+      query.whereRaw(findParams.join(' AND '));
+    }
+
+    return await query;
+  },
+
+  findAll: async () => {
+    return await database(table).select('*');
+  },
+
+  create: async (comodoAcessoData) => {
+    const query = database(table).insert(comodoAcessoData);
+    return await query;
+  },
+
+  update: async (id_acesso, comodoAcessoData) => {
+    const query = database(table).where('id_acesso', id_acesso).update(comodoAcessoData);
+    return await query;
+  },
+
+  delete: async (id_acesso) => {
+    const query = database(table).where('id_acesso', id_acesso).del();
+    return await query;
+  },
+};
